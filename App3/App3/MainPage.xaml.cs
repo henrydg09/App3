4d7c25dd1517742e4248003fe367f21aee234694
henrydg09 <henrydg09@gmail.com>
2019-04-04 10:29:23 -0300

    Versão inicial

--- App3/App3/MainPage.xaml.cs
+++ App3/App3/MainPage.xaml.cs
@@ -7,11 +7,61 @@ using Xamarin.Forms;
 
 namespace App3
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        // vetor de disciplinas
+        Disciplina[] disciplinas = new Disciplina[4]
+        {
+            new Disciplina("Cálculo 1", 1),
+            new Disciplina("Introdução a Engenharia", 2),
+            new Disciplina("Cálculo 2", 3),
+            new Disciplina("Projetos de Engenharia", 4)
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+            // para todos as disciplinas do vetor
+            foreach (Disciplina disciplina in disciplinas)
+            {
+                // adicionar um elemento na caixa de seleção
+                Picker.Items.Add(disciplina.semestre + " - " + disciplina.nome);
+            }
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            // desabilitar mensagens
+            Label1.IsVisible = false;
+            Label2.IsVisible = false;
+            // verificar se período e semestre foram selecionados
+            if (Entry2.Text != null &&
+                Entry2.Text.Length > 0 &&
+                Picker.SelectedIndex >= 0)
+            {
+                // criar objeto aluno
+                Aluno aluno = new Aluno();
+                // modificar o período do aluno de acordo com o texto
+                aluno.periodo = int.Parse(Entry2.Text);
+                // verificar se os valores são iguais
+                if (aluno.periodo == disciplinas[Picker.SelectedIndex].semestre)
+                {
+                    // habilitar mensagem de erro
+                    Label1.IsVisible = true;
+                }
+                else
+                {
+                    // habilitar mensagem de erro
+                    Label2.IsVisible = true;
+                }
+            }
+            else
+            {
+                // habilitar mensagem de erro
+                Label2.IsVisible = true;
+            }
+
+        }
+
+    }
 }
