a7752d172388172d0e962464decd6299e4bfdd16
henrydg09 <henrydg09@gmail.com>
2019-04-09 10:38:06 -0300

    versão estruturada

--- App3/App3/MainPage.xaml.cs
+++ App3/App3/MainPage.xaml.cs
@@ -10,8 +10,60 @@ namespace App3
 	public partial class MainPage : ContentPage
 	{
 		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+        {
+            InitializeComponent();
+            
+        }
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            Label1.IsVisible = false;
+            Label2.IsVisible = false;
+            Label3.IsVisible = false;
+            Label4.IsVisible = false;
+
+            if (Entry1.Text != null &&
+                Entry2.Text != null &&
+                Entry1.Text.Length > 0 &&
+                Entry2.Text.Length > 0 &&
+                Picker.SelectedIndex >= 0 &&
+                Picker2.SelectedIndex >= 0)
+            {
+                if (Picker.SelectedIndex == Picker2.SelectedIndex)
+                {
+                    int Matricula = int.Parse(Entry1.Text);
+                    int Nota = int.Parse(Entry2.Text);
+                    if (Nota >= 6)
+                    {
+                        Label1.IsVisible = true;
+                        Label3.IsVisible = true;
+
+                    }
+                    else
+                    {
+                        Label1.IsVisible = true;
+                        Label4.IsVisible = true;
+
+                    }
+
+
+
+
+                }
+
+
+
+            }
+            else
+            {
+
+                Label2.IsVisible = true;
+
+            }
+
+
+
+
+
+        }
+    }
 }
