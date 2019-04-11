02d10995a168e626cf1a44bf0e3e656132898889
henrydg09 <henrydg09@gmail.com>
2019-04-11 12:09:31 -0300

    Orientacao objeto

--- App3/App3/MainPage.xaml.cs
+++ App3/App3/MainPage.xaml.cs
@@ -4,15 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using App3.modelos;
+
 
 namespace App3
 {
 	public partial class MainPage : ContentPage
 	{
+        // vetor de Professor
+        Professor[] professores = new Professor[4]
+        {
+            new Professor("Maria",1),
+            new Professor("José",2),
+            new Professor("Gabriel",3),
+            new Professor("Marcia",4)
+        };
+        // vetor de  Disciplinas 
+        Disciplina[] disciplinas = new Disciplina[4]
+        {
+            new Disciplina("Calculo 1",1,1),
+            new Disciplina("Física",2,2),
+            new Disciplina("Programação",3,3),
+            new Disciplina("Calculo 2",4,4)
+        };
+
 		public MainPage()
         {
             InitializeComponent();
-            
+            //para todos professores do vetor
+            foreach (Professor professor in professores)
+            {
+                Picker2.Items.Add(professor.codigo + " - " + professor.nome);
+            }
+
+            foreach (Disciplina disciplina in disciplinas)
+            {
+                Picker.Items.Add(disciplina.professor + " - " + disciplina.nome);
+            }
         }
         void OnButtonClicked(object sender, EventArgs args)
         {
@@ -28,15 +56,17 @@ namespace App3
                 Picker.SelectedIndex >= 0 &&
                 Picker2.SelectedIndex >= 0)
             {
-                if (Picker.SelectedIndex == Picker2.SelectedIndex)
+                Disciplina disciplina = disciplinas[Picker.SelectedIndex];
+                Professor professor = professores[Picker2.SelectedIndex];
+                if (disciplina.Lecionar(professor))
                 {
-                    int Matricula = int.Parse(Entry1.Text);
-                    int Nota = int.Parse(Entry2.Text);
-                    if (Nota >= 6)
+                    Aluno aluno = new Aluno();
+                    aluno.matricula = Entry1.Text;
+                    Nota nota = new Nota(disciplina.nome, aluno.matricula, int.Parse(Entry2.Text));
+                    if (nota.Aprovar())
                     {
                         Label1.IsVisible = true;
                         Label3.IsVisible = true;
-
                     }
                     else
                     {
@@ -44,25 +74,14 @@ namespace App3
                         Label4.IsVisible = true;
 
                     }
-
-
-
-
                 }
-
-
-
-            }
-            else
+                else {
+                    Label2.IsVisible = true;
+                }
+            }else
             {
-
                 Label2.IsVisible = true;
-
             }
-
-
-
-
 
         }
     }
